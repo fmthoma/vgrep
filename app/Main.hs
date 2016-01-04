ba8d913568cd51394b4e9b28bdc88648dd5087ad
Franz Thoma <franz.thoma@tngtech.com>
2016-01-05 00:04:44 +0100

    working event loop

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,30 @@
 module Main where
 
-import Lib
+import Data.Foldable
+import Data.List
+import Graphics.Vty
 
 main :: IO ()
-main = someFunc
+main = do
+    cfg <- standardIOConfig
+    vty <- mkVty cfg
+    ls <- fmap lines (readFile "vgrep.cabal")
+    eventLoop vty ls 0
+    shutdown vty
+
+eventLoop :: Vty -> [String] -> Int -> IO ()
+eventLoop vty ls ln = do
+    let dpls = fmap fore (take ln ls)
+            ++ [ back (ls !! ln) ]
+            ++ fmap fore (drop (ln + 1) ls)
+        img = fold dpls
+        pic = picForImage img
+    update vty pic
+    e <- nextEvent vty
+    case e of
+        EvKey KUp    [] -> eventLoop vty ls (if ln == 0 then 0 else ln - 1)
+        EvKey KDown  [] -> eventLoop vty ls (if ln < length ls - 1 then ln + 1 else length ls - 1)
+        EvKey _      [] -> return ()
+  where
+    fore = string (defAttr `withForeColor` green) . (' ':)
+    back = string (defAttr `withBackColor` blue) . (' ':)
