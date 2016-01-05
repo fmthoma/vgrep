6c6375a2801c2935124c419c3a09decb2fcad7a7
Franz Thoma <franz.thoma@tngtech.com>
2016-01-05 01:29:44 +0100

    Switch to Data.Sequence

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@
 module Main where
 
 import Data.Foldable
-import Data.List
+import Data.Sequence hiding (update)
 import Graphics.Vty
 import System.Posix
 
@@ -12,18 +12,20 @@ main = do
     cfg <- standardIOConfig
     tty <- openFd "/dev/tty" ReadOnly Nothing defaultFileFlags
     vty <- mkVty (cfg { inputFd = Just tty })
-    eventLoop (PagerState ls 0 vty)
+    eventLoop $ PagerState { bufferPre   = empty
+                           , currentLine = head ls
+                           , bufferPost  = fromList (tail ls)
+                           , display     = vty }
     shutdown vty
 
-data PagerState = PagerState { lineBuffer  :: [String]
-                             , currentLine :: Int
+data PagerState = PagerState { bufferPre   :: Seq String
+                             , currentLine :: String
+                             , bufferPost  :: Seq String
                              , display     :: Vty }
 
 eventLoop :: PagerState -> IO ()
 eventLoop state@PagerState{..} = do
-    let dpls = fmap fore (take currentLine lineBuffer)
-            ++ [ back (lineBuffer !! currentLine) ]
-            ++ fmap fore (drop (currentLine + 1) lineBuffer)
+    let dpls = (fmap fore bufferPre |> back currentLine) >< fmap fore bufferPost
         img = fold dpls
         pic = picForImage img
     update display pic
@@ -37,13 +39,15 @@ eventLoop state@PagerState{..} = do
     back = string (defAttr `withBackColor` blue) . (' ':)
 
 nextLine :: PagerState -> PagerState
-nextLine state = let line = currentLine state + 1
-                 in  if line < length (lineBuffer state)
-                         then state { currentLine = line }
-                         else state { currentLine = length (lineBuffer state) - 1 }
+nextLine state@PagerState{..} = case viewl bufferPost of
+    EmptyL  -> state
+    l :< ls -> state { bufferPre   = bufferPre |> currentLine
+                     , currentLine = l
+                     , bufferPost  = ls }
 
 previousLine :: PagerState -> PagerState
-previousLine state = let line = currentLine state - 1
-                     in  if line >= 0
-                             then state { currentLine = line }
-                             else state { currentLine = 0 }
+previousLine state@PagerState{..} = case viewr bufferPre of
+    EmptyR  -> state
+    ls :> l -> state { bufferPre   = ls
+                     , currentLine = l
+                     , bufferPost  = currentLine <| bufferPost }
