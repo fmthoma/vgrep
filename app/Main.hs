f3112c12bf7594170ff3393144c082209842d7dd
Franz Thoma <franz.thoma@tngtech.com>
2016-01-07 13:16:34 +0100

    Split view as main widget

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,13 @@
 module Main where
 
 import Data.Monoid
+import Data.Ratio
 import Graphics.Vty hiding (resize)
 
 import Vgrep.App
 import Vgrep.Event
 import Vgrep.Widget as Widget
+import Vgrep.Widget.HorizontalSplit
 import Vgrep.Widget.Pager
 
 main :: IO ()
@@ -15,23 +17,28 @@ main = do
     return ()
 
 
-app :: App PagerWidget
-app = App { initialize  = initPager
+type MainWidget = HSplitWidget PagerWidget PagerWidget
+
+app :: App MainWidget
+app = App { initialize  = initSplitView
           , handleEvent = eventHandler
           , render      = picForImage . drawWidget }
 
-initPager :: Vty -> IO PagerWidget
-initPager vty = do
+initSplitView :: Vty -> IO MainWidget
+initSplitView vty = do
     ls <- fmap lines (getContents)
     displayRegion <- displayBounds (outputIface vty)
-    return (pagerWidget ls displayRegion)
+    let leftPager  = pagerWidget ls displayRegion
+        rightPager = pagerWidget (reverse ls) displayRegion
+    displayRegion <- displayBounds (outputIface vty)
+    return (hSplitWidget leftPager rightPager (1 % 3) displayRegion)
 
-eventHandler :: EventHandler PagerWidget
+eventHandler :: EventHandler MainWidget
 eventHandler = exitOn (KChar 'q') []
             <> handleResizeEvent
             <> passEventsToWidget
 
-handleResizeEvent :: EventHandler PagerWidget
+handleResizeEvent :: EventHandler MainWidget
 handleResizeEvent = EventHandler $ \event widget -> case event of
     EvResize w h -> return . Continue $ resizeWidget widget (w, h)
     _            -> return Unchanged
--- app/Vgrep/Widget/HorizontalSplit.hs
+++ app/Vgrep/Widget/HorizontalSplit.hs
@@ -8,48 +8,59 @@ import Graphics.Vty.Prelude
 import Vgrep.Event
 import Vgrep.Widget.Type
 
-data HSplitState s t = State { widgets :: (Widget s, Widget t)
+data HSplitState s t = State { widgets :: (s, t)
                              , focused :: Focus
                              , ratio   :: Rational
                              , region  :: DisplayRegion }
 
 data Focus = FocusLeft | FocusRight
 
-_widgets :: Lens' (HSplitState s t) (Widget s, Widget t)
+_widgets :: Lens' (HSplitState s t) (s, t)
 _widgets = lens widgets
                 (\state widgets' -> state { widgets = widgets' })
 
-leftWidget :: Lens' (HSplitState s t) (Widget s)
+leftWidget :: Lens' (HSplitState s t) s
 leftWidget = _widgets . _1
 
-rightWidget :: Lens' (HSplitState s t) (Widget t)
+rightWidget :: Lens' (HSplitState s t) t
 rightWidget = _widgets . _2
 
-type HSplitWidget = forall s t. Widget (HSplitState s t)
+type HSplitWidget s t = Widget (HSplitState s t)
 
 hSplitWidget :: Widget s
              -> Widget t
              -> Rational
              -> DisplayRegion
-             -> Widget (HSplitState s t)
+             -> HSplitWidget (Widget s) (Widget t)
 hSplitWidget left right ratio region =
-    Widget { state       = State (left, right) FocusLeft ratio region
+    Widget { state       = initState left right ratio region
            , dimensions  = region
            , resize      = resizeWidgets
            , draw        = drawWidgets
            , handleEvent = passEventToFocusedWidget }
 
+initState :: Widget s
+          -> Widget t
+          -> Rational
+          -> DisplayRegion
+          -> HSplitState (Widget s) (Widget t)
+initState left right ratio region = resizeWidgets region $
+    State { widgets = (left, right)
+          , focused = FocusLeft
+          , ratio   = ratio
+          , region  = region }
 
-passEventToFocusedWidget :: EventHandler (HSplitState s t)
+
+passEventToFocusedWidget :: EventHandler (HSplitState (Widget s) (Widget t))
 passEventToFocusedWidget = EventHandler $ \event state@State{..} ->
     case focused of
         FocusLeft  -> passEventTo leftWidget  event state
         FocusRight -> passEventTo rightWidget event state
 
-passEventTo :: Lens' (HSplitState s t) (Widget u)
+passEventTo :: Lens' (HSplitState (Widget s) (Widget t)) (Widget u)
             -> Event
-            -> HSplitState s t
-            -> IO (Next (HSplitState s t))
+            -> HSplitState (Widget s) (Widget t)
+            -> IO (Next (HSplitState (Widget s) (Widget t)))
 passEventTo selector event =
     over (liftNext selector) (handle passEventsToWidget event)
 
@@ -58,14 +69,16 @@ liftNext :: Lens' s a
 liftNext l = lens (view l)
                   (\s -> fmap (fmap (\a -> set l a s)))
 
-resizeWidgets :: DisplayRegion -> HSplitState s t -> HSplitState s t
+resizeWidgets :: DisplayRegion
+              -> HSplitState (Widget s) (Widget t)
+              -> HSplitState (Widget s) (Widget t)
 resizeWidgets newRegion@(w, h) state@State{..} =
     let leftRegion  = (ceiling (ratio * fromIntegral w), h)
-        rightRegion = (floor   (ratio * fromIntegral w), h)
+        rightRegion = (floor ((1 - ratio) * fromIntegral w), h)
     in  state { widgets = ( resizeWidget (fst widgets) leftRegion
                           , resizeWidget (snd widgets) rightRegion )
               , region = newRegion }
 
-drawWidgets :: HSplitState s t -> Image
+drawWidgets :: HSplitState (Widget s) (Widget t) -> Image
 drawWidgets state@State{..} = drawWidget (view leftWidget  state)
-                          <-> drawWidget (view rightWidget state)
+                          <|> drawWidget (view rightWidget state)
