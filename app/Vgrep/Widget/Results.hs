eda3e6f63a3f7c4ee82265b4980f2154b850bf48
Franz Thoma <franz.thoma@tngtech.com>
2016-01-08 00:16:40 +0100

    Scroll positions for result list

--- app/Vgrep/Widget/Results.hs
+++ app/Vgrep/Widget/Results.hs
@@ -1,12 +1,14 @@
-{-# LANGUAGE TemplateHaskell, DisambiguateRecordFields #-}
+{-# LANGUAGE TemplateHaskell, DisambiguateRecordFields, MultiWayIf #-}
 module Vgrep.Widget.Results where
 
-import Control.Lens
+import Control.Lens (over, set, view, _2, (&))
 import Control.Lens.TH
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Data.Sequence as Seq hiding (length)
-import Graphics.Vty (DisplayRegion, Image)
+import Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
+import Graphics.Vty (Image)
+import Graphics.Vty.Prelude
 
 import Vgrep.Widget.Type
 
@@ -15,15 +17,19 @@ data Buffer a = Buffer { _size :: !Int
                        , _cur  :: !a
                        , _post :: !(Seq a) }
 
-data ResultsState = State { _files      :: Buffer FileResults
-                          , _cursorPos  :: Int
-                          , _dimensions :: DisplayRegion }
+makeLenses ''Buffer
+
+
+data ResultsState = State { _files     :: Buffer FileResults
+                          , _scrollPos :: Int
+                          , _region    :: DisplayRegion }
 
 type FileResults = (String, Buffer Line)
 type Line = (LineNumber, String)
 type LineNumber = Int
 
 makeLenses ''ResultsState
+
 
 type ResultsWidget = Widget ResultsState
 
@@ -47,8 +53,30 @@ initState :: Map String [Line]
           -> DisplayRegion
           -> ResultsState
 initState files dimensions =
-    State { _files      = filesBuffer files
-          , _cursorPos  = 0
-          , _dimensions = dimensions }
+    State { _files     = buffer
+          , _scrollPos = 0
+          , _region    = dimensions }
   where
-    filesBuffer = initBuffer . map (over _2 initBuffer) . Map.toList
+    buffer = (initBuffer . map (over _2 initBuffer) . Map.toList) files
+
+updateScrollPos :: ResultsState -> ResultsState
+updateScrollPos state =
+    if | current < firstVisible -> state & set scrollPos current
+       | current > lastVisible  -> state & set scrollPos (current - height + 1)
+       | otherwise              -> state
+  where
+    height       = regionHeight (view region state)
+    current = computeCurrentItem (view files state)
+    firstVisible = view scrollPos state
+    lastVisible  = firstVisible + height - 1
+
+computeCurrentItem :: Buffer FileResults -> Int
+computeCurrentItem buffer = linesInFilesBeforeCurrent
+                          + fileHeadersBeforeCurrent
+                          + linesInCurrentFileBeforeCursor
+  where
+    fileHeadersBeforeCurrent = length (view pre buffer)
+    linesInFilesBeforeCurrent =
+        (sum . fmap length) (view (pre . traverse . _2 . pre) buffer)
+    linesInCurrentFileBeforeCursor =
+        (sum . fmap length) (view (cur . _2 . pre) buffer)
