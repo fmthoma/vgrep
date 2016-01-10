d5394196c1511da209ea2b7f59107e8539d26328
Franz Thoma <franz.thoma@tngtech.com>
2016-01-11 00:36:11 +0100

    First parser implementation

---
+++ src/Vgrep/Parser.hs
@@ -0,0 +1,19 @@
+module Vgrep.Parser () where
+
+import Control.Applicative
+import Data.Attoparsec.Text.Lazy
+import Data.Maybe
+import Data.Text.Lazy
+
+parseGrepOutput :: [Text] -> [(Text, Maybe Int, Text)]
+parseGrepOutput = catMaybes . fmap parseLine
+
+parseLine :: Text -> Maybe (Text, Maybe Int, Text)
+parseLine line = maybeResult (parse lineParser line)
+
+lineParser :: Parser (Text, Maybe Int, Text)
+lineParser = do
+    file       <- many (notChar ':') <* char ':'
+    lineNumber <- optional (decimal <* char ':')
+    result     <- takeLazyText
+    return (pack file, lineNumber, result)
