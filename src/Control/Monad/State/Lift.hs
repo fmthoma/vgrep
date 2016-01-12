0e6e75f96549f29a897008cf0338fd5a822689de
Franz Thoma <franz.thoma@tngtech.com>
2016-01-12 18:57:23 +0100

    No need for mmorph, this solution is even slightly more general

--- src/Control/Monad/State/Lift.hs
+++ src/Control/Monad/State/Lift.hs
@@ -1,8 +1,7 @@
 module Control.Monad.State.Lift where
 
-import Control.Monad.Morph
 import Control.Monad.State
 import Control.Monad.State.Class
 
-liftState :: Monad m => State s a -> StateT s m a
-liftState = hoist generalize
+liftState :: MonadState s m => State s a -> m a
+liftState = state . runState
