251f0945620216b9ddc052f0d8a1a78050d46318
David Luposchainsky <dluposchainsky@gmail.com>
2016-02-09 09:09:05 +0100

    Use manyTill

--- src/Vgrep/Parser.hs
+++ src/Vgrep/Parser.hs
@@ -19,7 +19,7 @@ parseLine line = maybeResult (parse lineParser line)
 
 lineParser :: Parser FileLineReference
 lineParser = do
-    file       <- many (notChar ':') <* char ':'
+    file       <- manyTill anyChar (char ':')
     lineNumber <- optional (decimal <* char ':')
     result     <- takeLazyText
     return (File (pack file), (lineNumber, result))
