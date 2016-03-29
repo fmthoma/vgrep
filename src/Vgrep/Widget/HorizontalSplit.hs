0cc157afbc975ae83bfba3d7a60926432b625c73
Franz Thoma <franz.thoma@tngtech.com>
2016-03-29 20:36:46 +0200

    Apply to HSplitWidget

--- src/Vgrep/Widget/HorizontalSplit.hs
+++ src/Vgrep/Widget/HorizontalSplit.hs
@@ -20,20 +20,25 @@ module Vgrep.Widget.HorizontalSplit
     ) where
 
 import Control.Lens
-import Control.Monad.State.Extended (State)
+import Control.Monad.State.Extended (State, StateT, get)
+import Control.Monad.Reader (local)
 import Graphics.Vty.Image hiding (resize)
-import Graphics.Vty.Prelude
 
+import Vgrep.Environment
 import Vgrep.Type
 import Vgrep.Widget.Type
 
 
 data HSplitState s t = State { _widgets :: (s, t)
-                             , _split   :: Split
-                             , _region  :: DisplayRegion }
+                             , _split   :: Split }
 
 data Focus = FocusLeft | FocusRight deriving (Eq)
 data Split = LeftOnly | RightOnly | Split (Focus, Rational) deriving (Eq)
+
+data HSplitEvent u v
+    = LeftEvent u
+    | RightEvent v
+    | BothEvents u v
 
 makeLenses ''HSplitState
 
@@ -66,65 +71,72 @@ rightWidgetFocused :: Traversal' (HSplitState s t) t
 rightWidgetFocused = currentWidget . _Right
 
 
-type HSplitWidget s t = Widget (HSplitState s t)
+type HSplitWidget u v s t = Widget (HSplitEvent u v) (HSplitState s t)
 
-hSplitWidget :: Widget s -> Widget t -> HSplitWidget s t
+hSplitWidget :: Widget u s -> Widget v t -> HSplitWidget u v s t
 hSplitWidget left right =
     Widget { initialize = initHSplit    left right
-           , resize     = resizeWidgets left right
-           , draw       = drawWidgets   left right }
+           , draw       = drawWidgets   left right
+           , handle     = handleEvents  left right }
 
-initHSplit :: Widget s -> Widget t -> DisplayRegion -> HSplitState s t
-initHSplit left right initialRegion =
-    State  { _widgets = ( initialize left  initialRegion
-                        , initialize right initialRegion )
-           , _split   = LeftOnly
-           , _region  = initialRegion }
+initHSplit :: Widget u s -> Widget v t -> HSplitState s t
+initHSplit left right =
+    State  { _widgets = (initialize left , initialize right)
+           , _split   = LeftOnly }
 
 
-leftOnly :: HSplitWidget s t -> State (HSplitState s t) ()
-leftOnly widget = do assign split LeftOnly
-                     use region >>= resize widget
+leftOnly :: State (HSplitState s t) ()
+leftOnly = assign split LeftOnly
 
-rightOnly :: HSplitWidget s t -> State (HSplitState s t) ()
-rightOnly widget = do assign split RightOnly
-                      use region >>= resize widget
+rightOnly :: State (HSplitState s t) ()
+rightOnly = assign split RightOnly
 
-splitFocusLeft :: Rational -> HSplitWidget s t -> State (HSplitState s t) ()
-splitFocusLeft ratio widget = do assign split (Split (FocusLeft, ratio))
-                                 use region >>= resize widget
+splitFocusLeft :: Rational -> State (HSplitState s t) ()
+splitFocusLeft ratio = do assign split (Split (FocusLeft, ratio))
 
-splitFocusRight :: Rational -> HSplitWidget s t -> State (HSplitState s t) ()
-splitFocusRight ratio widget = do assign split (Split (FocusRight, ratio))
-                                  use region >>= resize widget
+splitFocusRight :: Rational -> State (HSplitState s t) ()
+splitFocusRight ratio = assign split (Split (FocusRight, ratio))
 
-switchFocus :: HSplitWidget s t -> State (HSplitState s t) ()
-switchFocus widget = use split >>= \case
-    Split focus  -> do assign split (Split (switch focus))
-                       use region >>= resize widget
+switchFocus :: State (HSplitState s t) ()
+switchFocus = use split >>= \case
+    Split focus  -> assign split (Split (switch focus))
     _otherwise   -> pure ()
   where
     switch (FocusLeft,  ratio) = (FocusRight, 1 - ratio)
     switch (FocusRight, ratio) = (FocusLeft,  1 - ratio)
 
-resizeWidgets :: Widget s
-              -> Widget t
-              -> DisplayRegion
-              -> State (HSplitState s t) ()
-resizeWidgets left right newRegion@(w, h) = do
-    assign region newRegion
-    use split >>= \case
-        LeftOnly  -> zoom (widgets . _1) (resize left  newRegion)
-        RightOnly -> zoom (widgets . _2) (resize right newRegion)
-        Split (_, ratio) -> do
-            let leftRegion  = (ceiling (ratio * fromIntegral w), h)
-                rightRegion = (floor ((1 - ratio) * fromIntegral w), h)
-            zoom (widgets . _1) (resize left  leftRegion)
-            zoom (widgets . _2) (resize right rightRegion)
-
-drawWidgets :: Widget s -> Widget t -> HSplitState s t -> Vgrep Image
+drawWidgets :: Widget u s
+            -> Widget v t
+            -> HSplitState s t
+            -> Vgrep Image
 drawWidgets left right state = case view split state of
     LeftOnly  -> draw left  (view leftWidget  state)
     RightOnly -> draw right (view rightWidget state)
-    Split _   -> liftA2 (<|>) (draw left  (view leftWidget  state))
-                              (draw right (view rightWidget state))
+    Split _   -> liftA2 (<|>)
+        (local (leftRegion state)  (draw left  (view leftWidget  state)))
+        (local (rightRegion state) (draw right (view rightWidget state)))
+
+handleEvents :: Widget u s
+             -> Widget v t
+             -> HSplitEvent u v
+             -> StateT (HSplitState s t) Vgrep ()
+handleEvents left right = \case
+    LeftEvent l    -> handleLeft l
+    RightEvent r   -> handleRight r
+    BothEvents l r -> handleLeft l >> handleRight r
+  where handleLeft event = get >>= \state ->
+            local (leftRegion state) (zoom leftWidget (handle left event))
+        handleRight event = get >>= \state ->
+            local (rightRegion state) (zoom rightWidget (handle right event))
+
+leftRegion, rightRegion :: HSplitState s t -> Environment -> Environment
+leftRegion state = case view split state of
+    LeftOnly         -> id
+    RightOnly        -> id
+    Split (_, ratio) -> over region $ \(w, h) ->
+                            (ceiling (ratio * fromIntegral w), h)
+rightRegion state = case view split state of
+    LeftOnly         -> id
+    RightOnly        -> id
+    Split (_, ratio) -> over region $ \(w, h) ->
+                            (floor ((1 - ratio) * fromIntegral w), h)
