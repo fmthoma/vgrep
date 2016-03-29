731b0cfcd0181b820f1a65977f0c0eabd6ffcf10
Franz Thoma <franz.thoma@tngtech.com>
2016-03-29 20:05:38 +0200

    New Widget type

--- src/Vgrep/Widget/Type.hs
+++ src/Vgrep/Widget/Type.hs
@@ -2,10 +2,10 @@ module Vgrep.Widget.Type where
 
 import Control.Monad.State.Extended
 import Graphics.Vty.Image (Image)
-import Graphics.Vty.Prelude
 
 import Vgrep.Type
 
-data Widget s = Widget { initialize :: DisplayRegion -> s
-                       , resize     :: DisplayRegion -> State s ()
-                       , draw       :: s -> Vgrep Image }
+data Widget e s = Widget
+    { initialize :: s
+    , draw       :: s -> Vgrep Image
+    , handle     :: e -> StateT s Vgrep () }
