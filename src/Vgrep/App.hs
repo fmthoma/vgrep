4dcf43a11ba5272fc456e100e15386390edfb754
Franz Thoma <franz.thoma@tngtech.com>
2016-05-05 21:57:03 +0200

    Docs for Vgrep.App

--- src/Vgrep/App.hs
+++ src/Vgrep/App.hs
@@ -3,6 +3,8 @@
 module Vgrep.App
     ( App(..)
     , runApp, runApp_
+
+    -- * Auxiliary definitions
     , ttyIn
     , ttyOut
     ) where
@@ -23,16 +25,45 @@ import Vgrep.Environment
 import Vgrep.Event
 import Vgrep.Type
 
+
+-- | The 'App' type is parameterized over the type 'e' of events it handles
+-- and the type 's' of its state.
 data App e s = App
-    { initialize   :: forall m. MonadIO m => m s
-    , liftEvent    :: Vty.Event -> e
-    , handleEvent  :: forall m. MonadIO m => e -> s -> Next (VgrepT s m Redraw)
-    , render       :: forall m. Monad m => VgrepT s m Vty.Picture }
+    { initialize  :: forall m. MonadIO m => m s
+    -- ^ Creates the initial state for the app.
+
+    , liftEvent   :: Vty.Event -> e
+    -- ^ How to convert an external 'Vty.Event' to the App's event
+
+    , handleEvent :: forall m. MonadIO m => e -> s -> Next (VgrepT s m Redraw)
+    -- ^ Handles an event, possibly modifying the App's state.
+    --
+    -- @
+    -- handleEvent e s = case e of
+    --     'Vty.EvKey' 'Vty.KEnter' [] -> 'Continue' ('pure' 'Unchanged')
+    --     -- Handles the @Enter@ key, but does nothing.
+    --
+    --     'Vty.EvKey' 'Vty.KUp' [] -> 'Continue' ('pure' 'Redraw')
+    --     -- Handles the @Up@ key and triggers a redraw.
+    --
+    --     _otherwise          -> 'Skip'
+    --     -- Does not handle the event, so other handlers may be invoked.
+    -- @
+
+    , render      :: forall m. Monad m => VgrepT s m Vty.Picture
+    -- ^ Creates a 'Vty.Picture' to be displayed. May modify the App's
+    -- state (e. g. for resizing).
+    }
 
 
+-- | Like 'runApp', but does not return the final state.
 runApp_ :: App e s -> Config -> Producer e IO () -> IO ()
 runApp_ app conf externalEvents = void (runApp app conf externalEvents)
 
+-- | Runs runs the event loop until an @'Interrupt' 'Halt'@ is encountered.
+-- Events handled with @'Interrupt' ('Suspend' action)@ will shut down
+-- 'Vty.Vty', run the action (e. g. invoking an external editor), and start
+-- 'Vty.Vty' again.
 runApp :: App e s -> Config -> Producer e IO () -> IO s
 runApp app conf externalEvents = withSpawn unbounded $ \(evSink, evSource) -> do
     displayRegion <- displayRegionHack
@@ -44,6 +75,10 @@ runApp app conf externalEvents = withSpawn unbounded $ \(evSink, evSource) -> do
     cancel externalEventThread
     pure finalState
 
+-- | We need the display region in order to initialize the app, which in
+-- turn will start 'Vty.Vty'. To resolve this circular dependency, we start
+-- once 'Vty.Vty' in order to determine the display region, and shut it
+-- down again immediately.
 displayRegionHack :: IO DisplayRegion
 displayRegionHack = bracket initVty Vty.shutdown $ \vty ->
         Vty.displayBounds (Vty.outputIface vty)
@@ -107,5 +142,9 @@ initVty = do
     Vty.mkVty (cfg { Vty.inputFd = Just fdIn , Vty.outputFd = Just fdOut })
 
 ttyIn, ttyOut :: IO Fd
+-- | Opens @/dev/tty@ read-only. Should be connected to the @stdin@ of
+-- a GUI process (e. g. 'Vty.Vty').
 ttyIn  = openFd "/dev/tty" ReadOnly  Nothing defaultFileFlags
+-- | Opens @/dev/tty@ write-only. Should be connected to the @stdout@ of
+-- a GUI process (e. g. 'Vty.Vty').
 ttyOut = openFd "/dev/tty" WriteOnly Nothing defaultFileFlags
