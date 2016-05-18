febdb147b3488f7c918940139d17e74fcf0a8683
Franz Thoma <franz.thoma@tngtech.com>
2016-05-18 23:08:57 +0200

    First test cases for Results widget

--- test/Test/Vgrep/Widget/Results.hs
+++ test/Test/Vgrep/Widget/Results.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE LambdaCase #-}
 module Test.Vgrep.Widget.Results (test) where
 
+import Control.Lens
 import Test.Case
 import Test.QuickCheck
 import Test.QuickCheck.Monadic
@@ -8,10 +10,41 @@ import Vgrep.Widget.Results.Testable
 
 test :: TestTree
 test = runTestCases "Results widget"
-    [ TestProperty
-        { description = "foo"
+    [ TestInvariant
+        { description = "Scrolling one line down and up keeps selected line"
+        , testData = arbitrary `suchThat` (not . lastLine)
+        , testCase = run (nextLine >> prevLine)
+        , invariant = selectedLine
+        }
+    , TestInvariant
+        { description = "Scrolling one page down and up keeps selected line"
         , testData = arbitrary
-        , testCase = get :: PropertyM (Vgrep Results) Results
-        , assertion = \val ->               pure True
+            `suchThat` lastLineOnScreen
+            `suchThat` \(results, env) -> linesBelowCurrent (> screenHeight env) results
+        , testCase =  run (pageDown >> pageUp)
+        , invariant = selectedLine
         }
     ]
+
+selectedLine :: Getter Results (Maybe FileLineReference)
+selectedLine = to $ \case
+    EmptyResults -> Nothing
+    Results _ _ c _ _ -> Just c
+
+linesBelowCurrent :: (Int -> Bool) -> Results -> Bool
+linesBelowCurrent p = \case
+    EmptyResults        -> p 0
+    Results _ _ _ ds es -> p (length ds + length es)
+
+screenHeight :: Environment -> Int
+screenHeight = view (region . to regionHeight)
+
+lastLineOnScreen :: (Results, Environment) -> Bool
+lastLineOnScreen (results, _env) = case results of
+    EmptyResults        -> True
+    Results _ _ _ ds _ -> null ds
+
+lastLine :: (Results, Environment) -> Bool
+lastLine (results, _env) = case results of
+    EmptyResults        -> True
+    Results _ _ _ ds es -> null ds && null es
