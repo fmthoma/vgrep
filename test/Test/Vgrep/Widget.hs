b247288abb3e8d114078bb497d35337410957d73
Franz Thoma <franz.thoma@tngtech.com>
2016-06-13 23:38:56 +0200

    stylish-haskell

--- test/Test/Vgrep/Widget.hs
+++ test/Test/Vgrep/Widget.hs
@@ -2,7 +2,7 @@ module Test.Vgrep.Widget (test) where
 
 import Test.Tasty
 
-import qualified Test.Vgrep.Widget.Pager as Pager
+import qualified Test.Vgrep.Widget.Pager   as Pager
 import qualified Test.Vgrep.Widget.Results as Results
 
 test :: TestTree
