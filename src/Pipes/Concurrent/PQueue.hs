843ec8978c3554d0c002b7fba5307b382c708114
Franz Thoma <franz.thoma@tngtech.com>
2016-06-15 22:03:43 +0200

    Pipes.Concurrent implementation for PQueue
    Since Pipes.Concurrent is limited to a certain set of mailbox
    implementations, we basically have to re-implement the spawn function.

---
+++ src/Pipes/Concurrent/PQueue.hs
@@ -0,0 +1,59 @@
+module Pipes.Concurrent.PQueue
+  ( spawn
+  , withSpawn
+  , module Export
+  ) where
+
+import           Control.Applicative
+import           Control.Concurrent.STM         as STM
+import qualified Control.Concurrent.STM.TPQueue as TPQueue
+import           Control.Exception              (bracket)
+import           Control.Monad
+import           Pipes.Concurrent               (Input (..), Output (..))
+import qualified Pipes.Concurrent               as Export hiding
+    ( Buffer (..)
+    , bounded
+    , latest
+    , newest
+    , spawn
+    , spawn'
+    , unbounded
+    , withSpawn
+    )
+
+
+spawn :: Ord p => IO (Output (p, a), Input a, STM ())
+spawn = do
+    q <- TPQueue.newTPQueueIO
+    sealed <- STM.newTVarIO False
+    let seal = STM.writeTVar sealed True
+
+    {- Use weak TVars to keep track of whether the 'Input' or 'Output' has been
+       garbage collected.  Seal the mailbox when either of them becomes garbage
+       collected.
+    -}
+    rSend <- STM.newTVarIO ()
+    void (STM.mkWeakTVar rSend (STM.atomically seal))
+    rRecv <- STM.newTVarIO ()
+    void (STM.mkWeakTVar rRecv (STM.atomically seal))
+
+    let sendOrEnd p a = do
+            b <- readTVar sealed
+            if b
+                then return False
+                else do
+                    TPQueue.writeTPQueue q p a
+                    return True
+        readOrEnd = (fmap Just (TPQueue.readTPQueue q)) <|> (do
+            b <- readTVar sealed
+            check b
+            return Nothing )
+        _send (p, a) = sendOrEnd p a <* readTVar rSend
+        _recv        = readOrEnd     <* readTVar rRecv
+    return (Output _send, Input _recv, seal)
+{-# INLINABLE spawn #-}
+
+withSpawn :: Ord p => ((Output (p, a), Input a) -> IO r) -> IO r
+withSpawn action = bracket spawn
+    (\(_, _, seal) -> atomically seal)
+    (\(output, input, _) -> action (output, input))
