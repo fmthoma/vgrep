c11e158b92c96ecb2074b6c3b2f226cc1e03fe63
Franz Thoma <franz.thoma@tngtech.com>
2016-07-25 15:55:12 +0200

    WIP: More parsing thigy

--- src/Vgrep/EdLine.hs
+++ src/Vgrep/EdLine.hs
@@ -1,24 +1,23 @@
 {-# LANGUAGE DeriveFunctor              #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE KindSignatures             #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE Rank2Types                 #-}
-{-# LANGUAGE RecordWildCards            #-}
 module Vgrep.EdLine (
+      CmdParserT ()
+    , parse
+    , complete
+
+    , anytoken
+    , shortOption
+    , longOption
+    , word
     ) where
 
 import           Control.Applicative
-import           Control.Monad.Cont
-import           Control.Monad.Cont.Class
-import           Control.Monad.IO.Class
+import           Control.Monad
 import           Control.Monad.Trans.Class
-import           Data.Map.Strict           (Map)
-import qualified Data.Map.Strict           as Map
 import           Data.Monoid
 import           Data.Text                 (Text)
 import qualified Data.Text                 as Text
-
-import Vgrep.Type
 
 
 newtype CmdParserT m a = CmdParserT (forall b. (a -> m (NextT m b)) -> m (NextT m b))
@@ -26,6 +25,7 @@ newtype CmdParserT m a = CmdParserT (forall b. (a -> m (NextT m b)) -> m (NextT 
 
 instance Monad m => Applicative (CmdParserT m) where
     pure a = CmdParserT ($ a)
+    (<*>) = ap
 
 instance Monad m => Monad (CmdParserT m) where
     CmdParserT m >>= f = CmdParserT (\k -> m (\a -> let CmdParserT m' = f a in m' k))
@@ -35,7 +35,7 @@ instance MonadTrans CmdParserT where
     lift ma = CmdParserT (ma >>=)
 
 
-data NextT (m :: * -> *) a
+data NextT m a
     = Get (m [Text]) (Text -> m (NextT m a))
     | Pure a
     | Fail Text
@@ -49,10 +49,13 @@ instance (Monad m, Alternative m) => Alternative (NextT m) where
     empty = Fail "No alternative" --FIXME
 
     Fail err1 <|> Fail err2 = Fail (Text.unlines [err1, err2])
-    Fail _ <|> other = other
-    other <|> Fail _ = other
+    Fail _    <|> other     = other
+    other     <|> Fail _    = other
 
-    Get c1 f1 <|> Get c2 f2 = Get (liftA2 mappend c1 c2)(\t -> f1 t <|> f2 t)
+    Get c1 f1 <|> Get c2 f2 = Get (liftA2 mappend c1 c2) (\t -> f1 t <|> f2 t)
+    Get c  f  <|> Pure a    = Get c (\t -> f t <|> pure (Pure a))
+
+    Pure a    <|> _         = Pure a
 
 instance Monad m => Monad (NextT m) where
     Fail e        >>= _ = Fail e
@@ -66,20 +69,31 @@ instance MonadTrans NextT where
 
 
 runNextT :: Monad m => NextT m a -> [Text] -> m (Either Text a)
-runNextT next tokens = case next of
+runNextT parser tokens = case parser of
     Fail e -> pure (Left e)
     Pure a -> pure (Right a)
-    Get comp next -> case tokens of
-        [] -> pure (Left "FIXME")
+    Get _ next -> case tokens of
+        []              -> pure (Left "FIXME")
         token : tokens' -> next token >>= \next' -> runNextT next' tokens'
 
 compNextT :: Monad m => NextT m a -> [Text] -> m [Text]
-compNextT next tokens = case next of
-    Get enum next -> case tokens of
-        [] -> enum
-        [lastToken] -> fmap (filter (lastToken `Text.isPrefixOf`)) enum
-        token : tokens' -> next token >>= \next' -> compNextT next' tokens
-    _otherwise      -> pure []
+compNextT parser tokens = case parser of
+    Get comp next -> case tokens of
+        []              -> comp
+        [lastToken]     -> fmap (filter (lastToken `Text.isPrefixOf`)) comp
+        token : tokens' -> next token >>= \next' -> compNextT next' tokens'
+    _otherwise -> pure []
+
+
+parse :: Monad m => CmdParserT m a -> Text -> m (Either Text a)
+parse (CmdParserT next) text = do
+    final <- next (pure . pure)
+    runNextT final (Text.words text)
+
+complete :: Monad m => CmdParserT m a -> Text -> m [Text]
+complete (CmdParserT next) text = do
+    final <- next (pure . pure)
+    compNextT final (Text.words text)
 
 
 anytoken :: Applicative m => CmdParserT m Text
@@ -87,17 +101,17 @@ anytoken = CmdParserT (pure . Get (pure []))
 
 word :: Monad m => Text -> CmdParserT m Text
 word w = do
-    w' <- anytoken
+    w' <- CmdParserT (pure . Get (pure [w]))
     if w == w' then pure w else fail "FIXME"
 
 select :: Monad m => m [Text] -> CmdParserT m Text
 select choices = do
     items <- lift choices
-    token <- anytoken
+    token <- CmdParserT (pure . Get (pure items))
     if token `elem` items then pure token else fail "FIXME"
 
-file :: MonadIO m => CmdParserT m Text
-file = select _
+-- file :: MonadIO m => CmdParserT m Text
+-- file = select _
 
 arg :: Applicative m => CmdParserT m Text
 arg = anytoken
@@ -107,10 +121,3 @@ shortOption opt = void (select (pure [ "-" <> Text.singleton opt ])) >> arg
 
 longOption :: Monad m => Text -> CmdParserT m Text
 longOption opt = void (select (pure [ "--" <> opt ])) >> arg
-
-
-parse :: Monad m => CmdParserT m a -> Text -> m (Either Text a)
-parse = _
-
-complete :: Monad m => CmdParserT m a -> Text -> m [Text]
-complete = _
