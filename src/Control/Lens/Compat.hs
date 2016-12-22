700b709c4cb5eca7e23d498a9b7f955f78ede9bd
Franz Thoma <franz.thoma@tngtech.com>
2016-12-22 14:06:32 +0100

    Review changes & simplification

--- src/Control/Lens/Compat.hs
+++ src/Control/Lens/Compat.hs
@@ -1,14 +1,3 @@
--- | In lens-4.14, the constraint @'Functor' f@ is missing from the definition
--- of 'to'. When compiling with GHC 8.0, this leads to warnings for definitions
--- like
---
--- @
--- foo :: Getter Bar Foo
--- foo = to fooFromBar
--- @
---
--- because of the redundant @'Functor' f@ constraint. This module exports an
--- alternative definition with the added @'Functor' f@ Constraint.
 module Control.Lens.Compat
   ( to
   , module Control.Lens
@@ -19,12 +8,22 @@ import qualified Control.Lens as Lens
 
 
 -- | Build an (index-preserving) 'Getter' from an arbitrary Haskell function.
+-- See "Control.Lens".'Lens.to' for details.
 --
--- Identical to "Control.Lens".'Lens.to' except for the additional constraint
+-- In <https://hackage.haskell.org/package/lens-4.14 lens-4.14>, the constraint
+-- @'Functor' f@ is missing from the definition of 'Lens.to'. When compiling
+-- with GHC 8.0, this leads to warnings for definitions like
+--
+-- @
+-- foo :: Getter Bar Foo
+-- foo = to fooFromBar
+-- @
+--
+-- because of the redundant @'Functor' f@ constraint. This definition is
+-- identical to "Control.Lens".'Lens.to' except for the additional constraint
 -- @'Functor' f@.
 to :: (Profunctor p, Functor f, Contravariant f) => (s -> a) -> Optic' p f s a
-to k = fakeFunctorConstraintToDisableGhc8WarningWithLens4_14 . Lens.to k
+to k = getter
   where
-    fakeFunctorConstraintToDisableGhc8WarningWithLens4_14 getter =
-        let _ = rmap (fmap undefined) getter
-        in  getter
+    getter = Lens.to k
+    _fakeFunctorConstraint = rmap (fmap undefined) . getter
